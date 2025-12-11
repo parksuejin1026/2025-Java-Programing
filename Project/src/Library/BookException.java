57c9ba855315d43a1d4ba6e5ecb6288b93a3ef69
SueJin <suejin1026suejin@gmail.com>
2025-12-11 18:05:37 +0900

    주석교체

--- Project/src/Library/BookException.java
+++ Project/src/Library/BookException.java
@@ -1,6 +1,7 @@
 package Library;
 
-public class BookException extends Exception { // 자바의 기본 구성 클래스 exception을 부모 클래스로 둠
+public class BookException extends Exception { 
+    // 자바의 기본 구성 클래스 exception을 부모 클래스로 둠
 
     public BookException(String message) { // 오류 메시지 저장
         super(message);
